71ed5c8130eb72dadb3783cccdbe0136238d76f4
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2021-06-25 20:48:42 +0700

    json

---
+++ Json/app/src/main/java/com/example/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,12 @@
+package com.example.jsonmoviedb;
+
+public class ApiEndpoint {
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=50b8774172fa82ef70d96fa60ed200b9";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
